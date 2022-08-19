08bbf2ed07266331cebfc3c5765ae85ac5c30ea2
A01709042 <a01709042@tec.mx>
2022-08-18 19:40:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ C:/Python39/python.exe -m venv venv
 & "venv/Scripts/Activate.ps1"
 python.exe -m pip install --upgrade pip
 
-transformers, discord, pandas, discord.py, torch, 
+python.exe -m pip install transformers, discord, pandas, discord.py, torch
 
 
 """
